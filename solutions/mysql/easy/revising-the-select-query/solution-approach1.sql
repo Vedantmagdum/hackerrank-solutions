97b74befb8aededbca261a25c0758509c6221c96
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-27 15:27:37 +0530

    ✅ Revising the Select Query I [mysql] — approach1

---
+++ solutions/mysql/easy/revising-the-select-query/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+-- Problem     Revising the Select Query I
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-27, 03:27 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT*FROM CITY WHERE CountryCode='USA' and population > 100000; 
