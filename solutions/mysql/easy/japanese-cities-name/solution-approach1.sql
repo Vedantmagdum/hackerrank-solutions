79b28bd0a6a0548b2c828074e4afbac6ba5699d4
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-28 09:19:23 +0530

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-28, 09:19 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
