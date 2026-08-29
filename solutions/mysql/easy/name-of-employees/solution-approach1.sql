432b3e45c9d6de4dba0e1dc7156980be38a7b920
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-29 10:45:33 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,15 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-29, 10:45 a.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+SELECT name FROM Employee ORDER BY name;
