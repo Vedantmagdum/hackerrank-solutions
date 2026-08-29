a2a9c2e2cd911e28ba59d1bb37aad13d1ff0ce52
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-29 10:42:39 +0530

    ✅ Weather Observation Station 5 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-5/solution-approach1.sql
@@ -0,0 +1,17 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=true
+-- Problem     Weather Observation Station 5
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-29, 10:42 a.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+SELECT CITY, LENGTH(CITY)FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
+
+SELECT CITY, LENGTH(CITY)FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
