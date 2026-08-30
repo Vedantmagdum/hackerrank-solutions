3c561382389f76c2c4f19fe4f9680fc4acf0b363
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-30 17:27:34 +0530

    ✅ Weather Observation Station 8 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-8/solution-approach1.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-8/problem?isFullScreen=true
+-- Problem     Weather Observation Station 8
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-30, 05:27 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[AEIOUaeiou].*[AEIOUaeiou]$';
