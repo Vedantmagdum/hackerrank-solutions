b9a5b80170d0f2efbef1e52bb366b2195be057d9
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-31 22:08:53 +0530

    ✅ Revising Aggregations - The Sum Function [mysql] — approach1

---
+++ solutions/mysql/easy/revising-aggregations-sum/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+-- Problem     Revising Aggregations - The Sum Function
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-31, 10:08 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT SUM(population)  FROM CITY WHERE DISTRICT = 'California';
