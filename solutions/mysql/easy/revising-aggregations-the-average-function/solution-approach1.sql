f2be6db89cb2fc4757d5aa8c460cccb8a2468406
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-31 22:11:58 +0530

    ✅ Revising Aggregations - Averages [mysql] — approach1

---
+++ solutions/mysql/easy/revising-aggregations-the-average-function/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+-- Problem     Revising Aggregations - Averages
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-31, 10:11 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT AVG(population) FROM CITY WHERE DISTRICT = 'California';
