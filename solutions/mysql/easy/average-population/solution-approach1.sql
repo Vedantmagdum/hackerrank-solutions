b992da4c59013cc1354e9a4dab194a17436b4239
Vedant Madan Magdum <magdumvedant878@gmail.com>
2026-08-31 22:14:51 +0530

    ✅ Average Population [mysql] — approach1

---
+++ solutions/mysql/easy/average-population/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Problem     Average Population
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-31, 10:14 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT FLOOR(AVG(population)) FROM CITY;
